9a148417bfc34a6c15602994c1f3e1fcb56ec3cd
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-03-18 16:22:18 +0100

    :bug: Set python version for taiga major version

--- update.sh
+++ update.sh
@@ -5,6 +5,11 @@ declare -A base=(
 	[alpine]='alpine'
 )
 
+declare -A python_versions=(
+	[3]='3.6'
+	[4]='3.7'
+)
+
 variants=(
 	alpine
 )
@@ -30,6 +35,7 @@ echo "update docker images"
 travisEnv=
 for latest in "${latests[@]}"; do
 	version=$(echo "$latest" | cut -d. -f1-2)
+	major=$(echo "$latest" | cut -d. -f1-1)
 
 	# Only add versions >= "$min_version"
 	if version_greater_or_equal "$version" "$min_version"; then
@@ -49,6 +55,7 @@ for latest in "${latests[@]}"; do
 
             # Replace the variables.
             sed -ri -e '
+                s/%%PYTHON_VERSION%%/'"${python_versions[$major]}"'/g;
                 s/%%VARIANT%%/'"$variant"'/g;
                 s/%%VERSION%%/'"$latest"'/g;
             ' "$dir/Dockerfile"
