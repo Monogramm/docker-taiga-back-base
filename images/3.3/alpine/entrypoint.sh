ab247fb99e02a25e3c33063fe967d60bd9385faa
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-03-27 13:11:21 +0100

    :bug: Fix DB check

--- images/3.3/alpine/entrypoint.sh
+++ images/3.3/alpine/entrypoint.sh
@@ -9,7 +9,10 @@ sleep $TAIGA_SLEEP
 # Setup and check database automatically if needed
 if [ -z "$TAIGA_SKIP_DB_CHECK" ]; then
   echo "Running database check"
-  DB_CHECK_STATUS=$(python /checkdb.py)
+  set +e
+  python /checkdb.py
+  DB_CHECK_STATUS=$?
+  set -e
 
   if [ $DB_CHECK_STATUS -eq 1 ]; then
     echo "Failed to connect to database server or database does not exist."
