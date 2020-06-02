f02cc822d941e9f7d0eb22610f2fef3516158757
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-06-03 01:21:38 +0200

    :wrench: Add debug setup for celery
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/5.0/alpine/celery_local.py
+++ images/5.0/alpine/celery_local.py
@@ -8,3 +8,9 @@ broker_url = 'amqp://' + os.getenv('RABBIT_USER', 'guest') + ':' + os.getenv('RA
 result_backend = 'redis://' + os.getenv('REDIS_HOST', 'localhost') + ':' + os.getenv('REDIS_PORT', '6379') + '/0'
 
 timezone = os.getenv('TAIGA_TIMEZONE', 'Europe/Madrid')
+
+if os.getenv('TAIGA_DEBUG').lower() == 'true':
+    print("Taiga debug enabled", file=sys.stderr)
+    DEBUG = True
+else:
+    DEBUG = False
